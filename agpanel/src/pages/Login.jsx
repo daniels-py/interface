a993c06941d1d471dd969c9c4c8b75ef1cc3ece0
daniels-py <danielramirezs5518@gmail.com>
2025-05-15 16:24:04 -0500

    style: diseño de login temporal

--- agpanel/src/pages/Login.jsx
+++ agpanel/src/pages/Login.jsx
@@ -1,23 +1,57 @@
 import { useNavigate } from 'react-router-dom';
 
+
+import './Login.css';
+
+import googleLogo from '../assets/google1.svg'
+import gitLogo from '../assets/git1.svg'
+
+
+
 const Login = () => {
   const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
-    // Aquí iría la lógica real de login.
     navigate('/dashboard');
   };
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Correo" required /><br />
-        <input type="password" placeholder="Contraseña" required /><br />
-        <button type="submit">Ingresar</button>
-      </form>
-      <p>¿No tienes cuenta? <a href="/register">Regístrate</a></p>
+    <div className="login-page">
+      <div className="container-form">
+      <img src={gitLogo} alt="Home" className="form-logo" />
+        <h2>Iniciar Sesión</h2>
+        <form onSubmit={handleLogin}>
+          <input
+            name="email"
+            placeholder="Correo electrónico"
+            className="input-field"
+          />
+          <div className="forgot-password">
+            <a href="">¿Has olvidado la contraseña?</a>
+          </div>
+          <input
+            name="password"
+            placeholder="Contraseña"
+            type="password"
+            className="input-field"
+          />
+          <button type="submit" className="login-btn">Entrar</button>
+        </form>
+        <div className="social-login">
+          <button className="social-btn google-btn">
+            <img src={googleLogo} alt="Google" className="social-icon" />
+            Iniciar con Google
+          </button>
+          <button className="social-btn github-btn">
+            <img src={gitLogo} alt="GitHub" className="social-icon" />
+            Iniciar con GitHub
+          </button>
+          <div className="create-account">
+            <a href="">Crear cuenta</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
